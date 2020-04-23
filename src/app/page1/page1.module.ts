fae95369d630ce543e1b460089919f706b105e85
trik <marco.marche@gnucoop.com>
2020-04-23 12:45:22 +0200

    module - new way - import from modules

--- src/app/page1/page1.module.ts
+++ src/app/page1/page1.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 
+import {DummyModule} from '../dummy';
 import {Page1Component} from './page1.component';
 
 @NgModule({
   declarations: [Page1Component],
   exports: [Page1Component],
+  imports: [DummyModule],
 })
 export class Page1Module { }
--- src/app/page2/page2.module.ts
+++ src/app/page2/page2.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 
+import {DummyModule} from '../dummy';
 import {Page2Component} from './page2.component';
 
 @NgModule({
   declarations: [Page2Component],
   exports: [Page2Component],
+  imports: [DummyModule],
 })
 export class Page2Module { }
